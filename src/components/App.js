bfd92138377ed3445b99b3b1540965cebb75e424
garciajordy <jordygarcia0119@gmail.com>
2021-05-24 17:37:46 +0300

    Convert App.js to class like requested

--- src/components/App.js
+++ src/components/App.js
@@ -1,27 +1,31 @@
-import { useState } from 'react';
+import React from 'react';
 import ButtonPanel from './ButtonPanel';
 import Display from './Display';
 import calculator from '../logic/calculate';
 
-function App() {
-  const [value, setValue] = useState({
-    total: null,
-    next: null,
-    operation: null,
-  });
-  const handleClick = (name) => {
-    const data = { ...value };
-    const result = calculator(data, name);
-    setValue({ ...result });
-  };
-  const { total, next } = value;
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
 
-  return (
-    <>
-      <Display result={(next && next.toString()) || (total && total.toString())} />
-      <ButtonPanel handleClick={handleClick} />
-    </>
-  );
+  render() {
+    const handleClick = (name) => {
+      const result = calculator(this.state, name);
+      this.setState({ ...result });
+    };
+    const { next, total } = this.state;
+    return (
+      <>
+        <Display result={(next && next.toString()) || (total && total.toString())} />
+        <ButtonPanel handleClick={handleClick} />
+      </>
+    );
+  }
 }
 
 export default App;
