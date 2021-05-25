b3a9433b6b564ed4acd1c52e28badfc02981960e
garciajordy <jordygarcia0119@gmail.com>
2021-05-25 14:40:01 +0300

    Add tests for App.js and snapshot

--- src/__tests__/components/App.test.js
+++ src/__tests__/components/App.test.js
@@ -1,9 +1,14 @@
-describe("Timeline", () => {
-    it("passing test", () => {
-      expect(true).toBeTruthy();
-    });
-  
-    it("failing test", () => {
-      expect(false).toBeTruthy();
-    });
-  });
+import React from 'react';
+import { render, cleanup, screen } from '@testing-library/react';
+import App from '../../components/App';
+
+afterEach(cleanup);
+
+it('should take a snapshot', () => {
+  const { asFragment } = render(<App />);
+  expect(asFragment(<App />)).toMatchSnapshot();
+});
+it('displays heading', () => {
+  render(<App />);
+  expect(screen.getByText('Ready to do some Maths?')).toBeInTheDocument();
+});
